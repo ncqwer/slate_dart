4500dbc37f40410236b55f0da24bacf89417d767
“ncqwer” <18694039767@163.com>
2021-05-13 14:06:18 +0800

    just for try

--- test/interface/path/common/root_test.dart
+++ test/interface/path/common/root_test.dart
@@ -2,7 +2,7 @@ import 'package:test/test.dart';
 import 'package:slate_dart/slate_dart.dart';
 
 void main() {
-  test('interface->path->common->root', () {
+  test('interface->path->common->root1111', () {
     final path = [0, 1, 2];
     final another = [3, 2];
     final output = Path.common(path, another);
